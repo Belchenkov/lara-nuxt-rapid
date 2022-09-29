460af9b645848816ed6339a6335a861ecd582553
Алексей Бельченков <u608110@gmail.com>
2022-09-29 16:28:19 +0400

    Make  Login Method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
@@ -18,6 +20,27 @@ class AuthController extends Controller
             ]
         );
 
-        return response($user, Response::HTTP_CREATED);
+        return response([
+            'status' => true,
+            'user' =>$user
+        ], Response::HTTP_CREATED);
+    }
+
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response([
+                'error' => 'invalid credentials',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        return response([
+            'status' => true,
+            'user' => auth()->user()
+        ])->withCookie(cookie(
+            'jwt',
+            auth()->user()?->createToken('token')->plainTextToken,
+            60 * 24
+        ));
     }
 }
