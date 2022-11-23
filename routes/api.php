6a572fadebb693971f185ecd9c8d99fec9a18d4a
Алексей Бельченков <u608110@gmail.com>
2022-11-23 18:14:17 +0400

    Creating Links

--- routes/api.php
+++ routes/api.php
@@ -40,10 +40,11 @@ Route::prefix('ambassador')->group(function () {
     Route::get('products/frontend', [ProductController::class, 'frontend']);
     Route::get('products/backend', [ProductController::class, 'backend']);
 
-    //Route::middleware(['auth:sanctum', 'scope.ambassador'])->group(function () {
+    Route::middleware(['auth:sanctum', 'scope.ambassador'])->group(function () {
         Route::get('stats', [StatsController::class, 'index']);
         Route::get('rankings', [StatsController::class, 'rankings']);
-    //});
+        Route::post('links', [LinkController::class, 'store']);
+    });
 });
 
 // Checkout
